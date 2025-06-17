97c5f7f8e5213db11b47c7e8438c81e3e9bf721c
Lukw07 <85254303+Lukw07@users.noreply.github.com>
2025-06-17 13:32:39 +0200

    Hotfix db.php

--- db.php
+++ db.php
@@ -1,12 +1,15 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'zskamenicka_rezv';
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
+$db   = getenv('DB_NAME');
+
+if (empty($host) || empty($user) || empty($pass) || empty($db)) {
+    die("Chybí nastavení databázových údajů v environment proměnných.");
+}
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Připojení k databázi selhalo: " . $conn->connect_error);
 }
-?>
